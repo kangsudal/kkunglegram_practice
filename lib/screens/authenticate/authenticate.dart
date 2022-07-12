7c8830a1bbae2ce8c41a28c5e6cfdd9450aa1d15
kangsudal_notebook <kangsudal@gmail.com>
2022-07-13 03:07:53 +0900

    Add NEXT button and google signup button

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -15,6 +15,44 @@ class LoginSignupScreen extends StatelessWidget {
         children: [
           TitleBackgroundImage(),
           TextFormFieldCard(),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.4 + 120,
+            right: 0,
+            left: 0,
+            child: Center(
+              child: Container(
+                height: 40,
+                width: 150,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("NEXT"),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height-125,
+            right: 0,
+            left: 0,
+            child: Column(
+              children: [
+                Text("or Signup with"),
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  label: Text("Google"),
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    minimumSize: Size(155,40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    backgroundColor: Palette.googleColor,
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
