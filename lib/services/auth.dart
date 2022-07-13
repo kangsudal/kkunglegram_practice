63cb7316c810c4fad04d07b975cf8d38cb0c51b7
kangsudal_notebook <kangsudal@gmail.com>
2022-07-13 18:17:57 +0900

    #4-Firebase Auth

---
+++ lib/services/auth.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anon
+  Future signInAnon() async {
+    try{
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+    }catch(e){
+      debugPrint(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email & password
+
+  // register with email & password
+
+  // sign out
+}
